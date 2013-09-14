78a8f5252b2b150d2110ea72d91e5d58965df6bd
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-09-14 16:38:51 -0400

    Adding cardinality and booleanInUse predicate to vertices

--- lab1/compiler/src/Compile/Types/Graph.hs
+++ lab1/compiler/src/Compile/Types/Graph.hs
@@ -4,6 +4,8 @@ import Data.Map
 
 data Vertex a = Vertex {vertexData :: a
                        ,vertexAdjacencies :: Map a ()
+                       ,vertexCardinality :: Int
+                       ,vertexIsLive :: Bool 
                        } deriving (Eq, Show)
 
 data Graph a = Graph (Map a (Vertex a)) deriving (Eq, Show)
