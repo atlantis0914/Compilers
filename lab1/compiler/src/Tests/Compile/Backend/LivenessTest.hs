b1622ff7f902c37b950fd5831359ead9c34e9215
= <pterxiao@gmail.com>
2013-09-15 03:41:53 -0400

    Added another test to liveness.

--- lab1/compiler/src/Tests/Compile/Backend/LivenessTest.hs
+++ lab1/compiler/src/Tests/Compile/Backend/LivenessTest.hs
@@ -1,4 +1,4 @@
-module Tests.Compile.Backend.LivenessTest (livenessTest) where 
+module Tests.Compile.Backend.LivenessTest (livenessTest) where
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
@@ -9,8 +9,8 @@ import Compile.Backend.Liveness
 
 return_test = assertEqual "liveness on a single return"
               [[ATemp 0]]
-              (liveness [AAsm {aAssign=[AReg 0], 
-                               aOp=Nop, 
+              (liveness [AAsm {aAssign=[AReg 0],
+                               aOp=Nop,
                                aArgs=[ALoc $ ATemp 0]}])
 
 live_test1 = assertEqual "liveness on a single return"
@@ -24,12 +24,36 @@ live_test1 = assertEqual "liveness on a single return"
                         AAsm {aAssign=[ATemp 2],
                               aOp=Add,
                               aArgs=[ALoc $ ATemp 0, ALoc $ ATemp 1]},
-                        AAsm {aAssign=[AReg 0], 
-                              aOp=Nop, 
+                        AAsm {aAssign=[AReg 0],
+                              aOp=Nop,
                               aArgs=[ALoc $ ATemp 2]}])
+
+t2aasmList =
+  [AAsm {aAssign = [ATemp 1], aOp = Nop, aArgs = [AImm 1]}
+  ,AAsm {aAssign = [ATemp 2], aOp = Nop, aArgs = [AImm 1]}
+  ,AAsm {aAssign = [ATemp 3], aOp = Add, aArgs = [ALoc(ATemp 2), ALoc(ATemp 1)]}
+  ,AAsm {aAssign = [ATemp 4], aOp = Add, aArgs = [ALoc(ATemp 3), ALoc(ATemp 2)]}
+  ,AAsm {aAssign = [ATemp 5], aOp = Add, aArgs = [ALoc(ATemp 4), ALoc(ATemp 3)]}
+  ,AAsm {aAssign = [ATemp 6], aOp = Nop, aArgs = [ALoc(ATemp 5)]}
+  ]
+
+t2alocsList =
+  [[]
+  ,[ATemp 1]
+  ,[ATemp 2, ATemp 1]
+  ,[ATemp 3, ATemp 2]
+  ,[ATemp 4, ATemp 3]
+  ,[ATemp 5]
+  ]
+
+large_test :: Assertion
+large_test = assertEqual "test large"
+        t2alocsList
+        (liveness t2aasmList)
 
 tests = [testCase "return_test" return_test
         ,testCase "live_test1" live_test1
+        ,testCase "large test" large_test
         ]
 
 livenessTest = tests
