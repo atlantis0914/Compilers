b9c4b23cddea6ee3d0a7007a7a68a9c8c423ed4a
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-09-14 22:48:01 -0400

    Adding maximum cardinality search

---
+++ lab1/compiler/src/Compile/Backend/MaximumCardinalitySearch.hs
@@ -0,0 +1,56 @@
+module Compile.Backend.MaximumCardinalitySearch where 
+
+import Compile.Types
+import Data.List 
+import Data.Map
+
+import Compile.Util.Graph
+
+-- Takes an interference graph and outputs a simplicial elimination ordering
+maximumCardinalitySearch :: Graph ALoc -> [ALoc]
+maximumCardinalitySearch (Graph g) = mcs (Graph g) (length (elems g)) []
+
+mcs :: Graph ALoc -> Int -> [ALoc] -> [ALoc]
+mcs (Graph g) n l = 
+  if (length l == n)
+  then reverse l 
+  else 
+    let
+      (g', Vertex {vertexData = v}) = chooseVertex (Graph g)
+    in 
+      mcs g' n (v:l)
+
+chooseVertex :: Graph ALoc -> (Graph ALoc, Vertex ALoc)
+chooseVertex g = 
+  let
+    (g', Just v) = chooseVertex' g
+  in
+    (g', v)
+
+chooseVertex' :: Graph ALoc -> (Graph ALoc, Maybe (Vertex ALoc))
+chooseVertex' (Graph g) = 
+  let 
+    v = elems g
+    f = \(vert@(Vertex {vertexIsLive = isLive})) ->
+        \(Graph g, res) -> 
+          if (not isLive) 
+            then (Graph g, res)
+            else case res of 
+                 Nothing -> updateGraphWithVert (Graph g) vert
+                 Just vert' -> updateGraphWithVert (Graph g) 
+                                                   (maxCardinality vert vert')
+  in
+    Data.List.foldr f (Graph g, Nothing) v
+  where updateGraphWithVert (Graph g) (vert@(Vertex {vertexData = v})) = 
+          let
+            g' = setVertexLiveTo (Graph g) v False
+            g'' = incrementOutCardinalities g' v 
+          in
+           (g'', Just vert) 
+    
+maxCardinality :: Vertex ALoc -> Vertex ALoc -> Vertex ALoc
+maxCardinality (v1@(Vertex {vertexCardinality = v1Card})) 
+               (v2@(Vertex {vertexCardinality = v2Card})) = 
+  case (v1Card `compare` v2Card) of
+    LT -> v2
+    otherwise -> v1
