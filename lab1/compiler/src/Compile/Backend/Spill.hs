e8d5764c95bd53e05dadd7c756714e26993b16ba
= <pterxiao@gmail.com>
2013-09-16 22:17:01 -0400

    Parse args in spill.

--- lab1/compiler/src/Compile/Backend/Spill.hs
+++ lab1/compiler/src/Compile/Backend/Spill.hs
@@ -8,14 +8,21 @@ spill aasms = concatMap spillAAsm aasms
 
 spillAAsm :: AAsm -> [AAsm]
 spillAAsm aasm@(AAsm {aAssign = [AReg i], aOp = op, aArgs = [arg]}) =
-  if i > max_color_num
-    then [AAsm {aAssign = [AReg spill_reg_num],
-                aOp = Nop,
-                aArgs = [ALoc $ AMem $ i - max_color_num]},
-          AAsm {aAssign = [AReg spill_reg_num],
-                aOp = op,
-                aArgs = [arg]},
-          AAsm {aAssign = [AMem $ i - max_color_num],
-                aOp = Nop,
-                aArgs = [ALoc $ AReg spill_reg_num]}]
-    else [aasm]
+  let
+    arg' = case arg of 
+             ALoc (AReg i) -> if i > max_color_num 
+                                then ALoc $ AMem $ i - max_color_num
+                                else arg
+             _ -> arg
+  in
+    if i > max_color_num
+      then [AAsm {aAssign = [AReg spill_reg_num],
+                  aOp = Nop,
+                  aArgs = [ALoc $ AMem $ i - max_color_num]},
+            AAsm {aAssign = [AReg spill_reg_num],
+                  aOp = op,
+                  aArgs = [arg']},
+            AAsm {aAssign = [AMem $ i - max_color_num],
+                  aOp = Nop,
+                  aArgs = [ALoc $ AReg spill_reg_num]}]
+      else [AAsm {aAssign = [AReg i], aOp = op, aArgs = [arg']}]
