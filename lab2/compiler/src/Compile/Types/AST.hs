90e6b871dd5ff8ba17b831e0ac382a19f9232900
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-09-29 17:19:12 -0400

    adding patterns to show

--- lab2/compiler/src/Compile/Types/AST.hs
+++ lab2/compiler/src/Compile/Types/AST.hs
@@ -100,6 +100,9 @@ instance Show Expr where
   show (ExpInt n _ _) = show n
   show (ExpBool b _) = show b
   show (ExpBinOp op e1 e2 _) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (ExpRelOp op e1 e2 _) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (ExpPolyEq op e1 e2 _) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (ExpLogOp op e1 e2 _) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
   show (Ident i _) = i
   show (ExpUnOp op e _) = show op ++ "(" ++ show e ++ ")"
   show (ExpTernary e1 e2 e3 _) = show e1 ++ " ? " ++ show e2 ++ " : " ++ show e3
