e73d4f8c2427b6ad5eccc907142abbe8fd6a22ad
Peter Xiao <pterxiao@gmail.com>
2013-09-29 21:48:20 -0400

    more live

--- lab2/compiler/src/Compile/Backend/LivePredicates.hs
+++ lab2/compiler/src/Compile/Backend/LivePredicates.hs
@@ -3,9 +3,11 @@ module Compile.Backend.LivePredicates where
 import Compile.Types
 import Data.List
 import qualified Data.Map as Map
+import qualified Data.Tuple as Tuple
 
 type LiveMap = Map.Map Int [ALoc]
 type LabelMap = Map.Map Int Int
+type LiveContext = (LiveMap, Bool)
 
 extractLocs :: [AVal] -> [ALoc]
 extractLocs [] = []
@@ -13,43 +15,52 @@ extractLocs (val:vals) =
   case val of ALoc loc -> loc : extractLocs vals
               _ -> extractLocs vals
 
-runPredicate :: LabelMap -> Int -> AAsm -> LiveMap -> LiveMap
-runPredicate _ i (AAsm {aAssign = [AReg 0], aArgs = args}) liveMap =
+runPredicate :: LabelMap -> Int -> AAsm -> LiveContext -> LiveContext
+runPredicate _ i (AAsm {aAssign = [AReg 0], aArgs = args}) (liveMap, isNew) =
   let
+    oldLocs = liveMap Map.! i
     locs = nub $ extractLocs args
+    isNew' = (length oldLocs) == (length locs)
     liveMap' = Map.insert i locs liveMap
   in
-    liveMap'
+    (liveMap', isNew || isNew')
 
-runPredicate _ i (AAsm {aAssign = assigns, aArgs = args}) liveMap =
+runPredicate _ i (AAsm {aAssign = assigns, aArgs = args}) (liveMap, isNew) =
   let
     locs = liveMap Map.! (i+1)
     oldLocs = liveMap Map.! i
     locs' = (nub $ extractLocs args) `union` (locs \\ assigns)
+    isNew' = (length oldLocs) == (length locs')
     liveMap' = Map.insert i locs' liveMap
   in
-    liveMap'
+    (liveMap', isNew || isNew')
 
-runPredicate _ i (ACtrl (ALabel _)) liveMap =
+runPredicate _ i (ACtrl (ALabel _)) (liveMap, isNew) =
   let
+    oldLocs = liveMap Map.! i
     locs = liveMap Map.! (i+1)
     liveMap' = Map.insert i locs liveMap
+    isNew' = (length oldLocs) == (length locs)
   in
-    liveMap'
+    (liveMap', isNew || isNew')
 
-runPredicate labelMap i (ACtrl (AIf aval label)) liveMap =
+runPredicate labelMap i (ACtrl (AIf aval label)) (liveMap, isNew) =
   let
+    oldLocs = liveMap Map.! i
     locs = liveMap Map.! (i+1)
     labelIndex = labelMap Map.! label
     locs' = locs `union` (liveMap Map.! labelIndex)
     liveMap' = Map.insert i locs' liveMap
+    isNew' = (length oldLocs) == (length locs')
   in
-    liveMap'
+    (liveMap', isNew || isNew')
 
-runPredicate labelMap i (ACtrl (AGoto label)) liveMap =
+runPredicate labelMap i (ACtrl (AGoto label)) (liveMap, isNew) =
   let
+    oldLocs = liveMap Map.! i
     labelIndex = labelMap Map.! label
     locs = liveMap Map.! labelIndex
     liveMap' = Map.insert i locs liveMap
+    isNew' = (length oldLocs) == (length locs)
   in
-    liveMap'
+    (liveMap', isNew || isNew')
