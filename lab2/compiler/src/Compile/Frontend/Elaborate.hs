644d977d170285691ee72dbfcde21014bd7b52e5
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-01 06:32:40 -0400

    Adding sketch of constant propagation for exprs - not robust enough to keep in future labs but it should be good enough to pass paulsons case

--- lab2/compiler/src/Compile/Frontend/Elaborate.hs
+++ lab2/compiler/src/Compile/Frontend/Elaborate.hs
@@ -73,22 +73,38 @@ elabParseBlock' (Block curStmts) ((PDecl s t pos Nothing):xs) =
                       ]
 elabParseBlock' (Block curStmts) (x:xs) = elabParseBlock' (Block (curStmts ++ [elabParseStmt x])) xs
 
-elabExpr :: Expr -> Expr 
-elabExpr e@(ExpBinOp o e1 e2 p) = checkExpr e $ ExpBinOp (verifyExprOp o p) (elabExpr e1) (elabExpr e2) p
-elabExpr e@(ExpRelOp o e1 e2 p) = checkExpr e $ ExpRelOp (verifyExprOp o p) (elabExpr e1) (elabExpr e2) p
-elabExpr e@(ExpPolyEq o e1 e2 p) = checkExpr e $ ExpPolyEq (verifyExprOp o p) (elabExpr e1) (elabExpr e2) p
-elabExpr e@(ExpLogOp And e1 e2 p) = checkExpr e $ ExpTernary (elabExpr e1) 
+elabExpr :: Expr -> Expr
+elabExpr e = let
+  e' = elabExpr' e
+  in foldExpr e'
+elabExpr' :: Expr -> Expr 
+elabExpr' e@(ExpBinOp o e1 e2 p) = checkExpr e $ ExpBinOp (verifyExprOp o p) (elabExpr e1) (elabExpr e2) p
+elabExpr' e@(ExpRelOp o e1 e2 p) = checkExpr e $ ExpRelOp (verifyExprOp o p) (elabExpr e1) (elabExpr e2) p
+elabExpr' e@(ExpPolyEq o e1 e2 p) = checkExpr e $ ExpPolyEq (verifyExprOp o p) (elabExpr e1) (elabExpr e2) p
+elabExpr' e@(ExpLogOp And e1 e2 p) = checkExpr e $ ExpTernary (elabExpr e1) 
                                              (elabExpr e2) 
                                              (ExpBool False p) p
-elabExpr e@(ExpLogOp Or e1 e2 p) = checkExpr e $ ExpTernary (elabExpr e1) 
+elabExpr' e@(ExpLogOp Or e1 e2 p) = checkExpr e $ ExpTernary (elabExpr e1) 
                                             (ExpBool True p)
                                             (elabExpr e2) p
-elabExpr e@(ExpUnOp o e1 p) = checkExpr e $ ExpUnOp (verifyExprOp o p) (elabExpr e1) p 
-elabExpr e@(ExpTernary e1 e2 e3 p) = checkExpr e $ ExpTernary (elabExpr e1) 
+elabExpr' e@(ExpUnOp o e1 p) = checkExpr e $ ExpUnOp (verifyExprOp o p) (elabExpr e1) p 
+elabExpr' e@(ExpTernary e1 e2 e3 p) = checkExpr e $ ExpTernary (elabExpr e1) 
                                               (elabExpr e2) 
                                               (elabExpr e3) p
+elabExpr' e = checkExpr e $ e
 
-elabExpr e = checkExpr e $ e
+foldExpr :: Expr -> Expr
+foldExpr e@(ExpBinOp Mul (ExpInt i1 _ b1) (ExpInt i2 _ _) p) = (ExpInt (i1 * i2) p b1)
+foldExpr e@(ExpBinOp Add (ExpInt i1 _ b1) (ExpInt i2 _ _) p) = (ExpInt (i1 + i2) p b1)
+foldExpr e@(ExpBinOp Sub (ExpInt i1 _ b1) (ExpInt i2 _ _) p) = (ExpInt (i1 - i2) p b1)
+foldExpr e@(ExpBinOp op e1 e2 p) = (ExpBinOp op (foldExpr e1) (foldExpr e2) p)
+foldExpr e@(ExpRelOp op e1 e2 p) = (ExpRelOp op (foldExpr e1) (foldExpr e2) p)
+foldExpr e@(ExpPolyEq op e1 e2 p) = (ExpPolyEq op (foldExpr e1) (foldExpr e2) p)
+foldExpr e@(ExpLogOp op e1 e2 p) = (ExpLogOp op (foldExpr e1) (foldExpr e2) p)
+foldExpr e@(ExpTernary e1 e2 e3 p) = (ExpTernary (foldExpr e1) (foldExpr e2) (foldExpr e3) p)
+foldExpr e@(ExpUnOp op e1 p) = (ExpUnOp op (foldExpr e1) p)
+foldExpr e = e
+
 
 checkExpr (ExpInt n p Dec) = 
   assert  (n <= (2^31)) (show n ++ " too large at " ++ show p)
