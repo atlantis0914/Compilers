63cda6a4d81bf75b823343cd33927bd76599a53b
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-09 04:04:42 -0400

    Removing AST.hs

--- lab3/compiler/src/Compile/Types/AST.hs
+++
@@ -1,9 +0,0 @@
-{- L1 Compiler
-   Author: Matthew Maurer <mmaurer@andrew.cmu.edu>
-   Modified by: Ryan Pearl <rpearl@andrew.cmu.edu>
-
-   Defines the AST we parse to
--}
-module Compile.Types.AST where
-
-
