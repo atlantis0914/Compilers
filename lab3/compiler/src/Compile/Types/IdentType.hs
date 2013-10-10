17a247770bca272030c379b13fe07662973363ec
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-09 23:29:40 -0400

    Adding initial identTypeMap for Elaboration

--- lab3/compiler/src/Compile/Types/IdentType.hs
+++ lab3/compiler/src/Compile/Types/IdentType.hs
@@ -1,7 +1,12 @@
 module Compile.Types.IdentType where
 
+import qualified Data.Map as Map
+
 data IdentType = IInt
                | IBool 
                | IVoid 
                | ITypeDef String deriving (Eq, Show)
 
+getIdentTypeMap = Map.fromList $ [("int", IInt), 
+                                  ("bool", IBool),
+                                  ("void", IVoid)]
