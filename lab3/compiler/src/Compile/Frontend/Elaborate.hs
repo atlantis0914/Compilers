44b6d5d50ec43bdfe7e7f295bc23226917f2206b
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-11 22:53:29 -0400

    Added typedef expansion to elaborate'

--- lab3/compiler/src/Compile/Frontend/Elaborate.hs
+++ lab3/compiler/src/Compile/Frontend/Elaborate.hs
@@ -9,13 +9,16 @@ import Compile.Util.IdentTypeUtil
 import Compile.Frontend.Expand
 import Compile.Frontend.ElaborateType
 
+
+-- type TypeDefs = Map.Map IdentType IdentType
+
 -- Takes a parse function list and elaborates it into a post-elab 
 -- function list. 
 elaborate :: ParseFnList -> Either String FnList
 -- elaborate (ParseFnList decls pos) = Right $ FnList (map elaboratePGDecl decls) pos
 elaborate (ParseFnList decls pos) = 
   let
-    (elab, map) = foldl elaboratePGDecls ([], Map.empty) decls
+    (elab, map) = foldl elaboratePGDecls ([], baseIdentTypeMap) decls
   in
     Right $ FnList elab pos
 
@@ -49,61 +52,76 @@ elaboratePGDecl (PFDefn (ParseFDefn {pfnName = name,
                                      pfnReturnType = return,
                                      pfnBody = body,
                                      pfnPos = pos}) defnPos) typeDefs = 
-  GFDefn (FDefn {fnName = name,
-                 fnArgs = args,
-                 fnArgTypes = argTypes,
-                 fnReturnType = return,
-                 fnBody = elaborateParseAST body,
-                 fnPos = pos}) defnPos
+  let
+    nName = checkTDIdent typeDefs name
+    nArgs = checkTDIdentList typeDefs args
+    nArgTypes = elaborateTDIdentTypes typeDefs argTypes 
+    nReturn = elaborateTDIdentType typeDefs return
+    nBody = elaborateParseAST typeDefs body
+  in
+    GFDefn (FDefn {fnName = nName,
+                   fnArgs = nArgs,
+                   fnArgTypes = nArgTypes,
+                   fnReturnType = nReturn,
+                   fnBody = nBody,
+                   fnPos = pos}) defnPos
 
 elaboratePGDecl (PFDecl (ParseFDecl name args argTypes return isL pos) dPos) typeDefs = 
-  GFDecl (FDecl name args argTypes return isL pos) dPos
+  let
+    nName = checkTDIdent typeDefs name
+    nArgs = checkTDIdentList typeDefs args
+    nArgTypes = elaborateTDIdentTypes typeDefs argTypes 
+    nReturn = elaborateTDIdentType typeDefs return
+  in
+    GFDecl (FDecl nName nArgs nArgTypes nReturn isL pos) dPos
 
 elaboratePGDecl (PTypeDef s1 s2 pos) typeDefs = GTypeDef s1 s2 pos
 
+
 -- Converts a parse level AST into a post-elaboration AST. Expects the encapsulated
 -- ParseStatement to be a PBlock. 
-elaborateParseAST (ParseAST (PBlock stmts) p) = 
+elaborateParseAST typedefs (ParseAST (PBlock stmts) p) = 
   let
     expanded = expandPStatements stmts
-    elaborated = elabParseBlock expanded
+    elaborated = elabParseBlock typedefs expanded
   in
     AST elaborated p
 
 -- Converts a parse-block into a single post-elaboration statement. 
-elabParseBlock :: [ParseStmt] -> Stmt
-elabParseBlock stmts = elabParseBlock' (Block []) stmts
+elabParseBlock :: TypeDefs -> [ParseStmt] -> Stmt
+elabParseBlock typedefs stmts = elabParseBlock' typedefs (Block []) stmts
 
 -- Converts a single parseStmt into a stmt, mutually calling elabParseBlock'
 -- if we observe a nested block, and elabParseCtrl if we observe a nested ctrl
-elabParseStmt :: ParseStmt -> Stmt
-elabParseStmt (PAsgn s a e p) = Asgn s a e p
-elabParseStmt (PDecl s t p Nothing) = Decl {declName = s, 
-                                            declTyp = t, 
-                                            declPos = p, 
-                                            declScope = SNop}
-elabParseStmt (PDecl s t p (Just _)) = error "shouldnt get here just"
-elabParseStmt (PCtrl c) = Ctrl (elabParseCtrl c)
-elabParseStmt (PExpr e) = Expr e
-elabParseStmt (PBlock stmts) = elabParseBlock' (Block []) stmts
+elabParseStmt :: TypeDefs -> ParseStmt -> Stmt
+elabParseStmt td (PAsgn s a e p) = Asgn s a e p
+elabParseStmt td (PDecl s t p Nothing) = 
+  Decl {declName = s, 
+        declTyp = elaborateTDIdentType td t, 
+        declPos = p, 
+        declScope = SNop}
+elabParseStmt td (PDecl s t p (Just _)) = error "shouldnt get here just"
+elabParseStmt td (PCtrl c) = Ctrl (elabParseCtrl td c)
+elabParseStmt td (PExpr e) = Expr e
+elabParseStmt td (PBlock stmts) = elabParseBlock' td (Block []) stmts
 
 -- Converts a Parse level control flow statement into a post-elab
 -- control flow statement, elaborating inner statements using elabParseStmt. 
-elabParseCtrl :: ParseCtrl -> Ctrl
-elabParseCtrl (If e ps1 ps2 pos) = If e (elabParseStmt ps1) (elabParseStmt ps2) pos
-elabParseCtrl (While e ps1 pos) = While e (elabParseStmt ps1) pos
-elabParseCtrl (Assert e pos) = Assert e pos
-elabParseCtrl (Return e pos) = Return e pos
+elabParseCtrl :: TypeDefs -> ParseCtrl -> Ctrl
+elabParseCtrl td (If e ps1 ps2 pos) = If e (elabParseStmt td ps1) (elabParseStmt td ps2) pos
+elabParseCtrl td (While e ps1 pos) = While e (elabParseStmt td ps1) pos
+elabParseCtrl td (Assert e pos) = Assert e pos
+elabParseCtrl td (Return e pos) = Return e pos
 
 -- Converts a list of parse level statements into a single statement. We
 -- recursively convert statements and append them to the post-elab block's 
 -- inner list. 
-elabParseBlock' :: Stmt -> [ParseStmt] -> Stmt 
-elabParseBlock' curblock [] = curblock
-elabParseBlock' (Block curStmts) ((PDecl s t pos Nothing):xs) = 
+elabParseBlock' :: TypeDefs -> Stmt -> [ParseStmt] -> Stmt 
+elabParseBlock' _ curblock [] = curblock
+elabParseBlock' td (Block curStmts) ((PDecl s t pos Nothing):xs) = 
   Block $ curStmts ++ [Decl {declName = s,
-                             declTyp = t,
+                             declTyp = elaborateTDIdentType td t,
                              declPos = pos,
-                             declScope = elabParseBlock' (Block []) xs}
+                             declScope = elabParseBlock' td (Block []) xs}
                       ]
-elabParseBlock' (Block curStmts) (x:xs) = elabParseBlock' (Block (curStmts ++ [elabParseStmt x])) xs
+elabParseBlock' td (Block curStmts) (x:xs) = elabParseBlock' td (Block (curStmts ++ [elabParseStmt td x])) xs
