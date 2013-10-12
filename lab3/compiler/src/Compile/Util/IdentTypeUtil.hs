c44fff7edc5336bde68942f04d5a6d6fe6c253da
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-11 22:54:24 -0400

    adding base ident-type map for use in expansion

--- lab3/compiler/src/Compile/Util/IdentTypeUtil.hs
+++ lab3/compiler/src/Compile/Util/IdentTypeUtil.hs
@@ -2,8 +2,16 @@ module Compile.Util.IdentTypeUtil where
 
 import Compile.Types
 
+import qualified Data.Map as Map
+
 toIdentType :: String -> IdentType
 toIdentType "int" = IInt
 toIdentType "bool" = IBool
 toIdentType "void" = IVoid
 toIdentType s = ITypeDef s
+
+tM = [(IInt, IInt),
+      (IBool, IBool),
+      (IVoid, IVoid)]
+
+baseIdentTypeMap = Map.fromList tM
