b8db1967cd3202c3cda4630e1e3a98c594a9a0f0
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-14 03:41:17 -0400

    Fixing osmium../tests1/osmium-derp-mismatch.l3

--- lab3/compiler/src/Compile/Frontend/ElaborateType.hs
+++ lab3/compiler/src/Compile/Frontend/ElaborateType.hs
@@ -3,17 +3,24 @@ module Compile.Frontend.ElaborateType where
 import Compile.Types
 import qualified Data.Map as Map
 
+import qualified Debug.Trace as Trace
+
 type TypeDefs = Map.Map IdentType IdentType
 
 checkTDIdent :: TypeDefs -> String -> String
 checkTDIdent typeDefs s = 
   -- Have to force s to ITypeDef to do the lookup
-  case (Map.lookup (ITypeDef s) typeDefs) of 
+  Trace.trace ("tdef : " ++ show typeDefs ++ " i " ++ s) $ case (Map.lookup (ITypeDef s) typeDefs) of 
     Just _ -> error ("Ident " ++ s ++ " has conflicting type-def")
     Nothing -> s
 
 checkTDIdentList :: TypeDefs -> [String] -> [String]
-checkTDIdentList typeDefs l = map (checkTDIdent typeDefs) l
+checkTDIdentList typeDefs l = 
+  if (ok)
+    then l
+    else error ("Conflicting type def")
+  where 
+    ok = all (\s -> (not $ Map.member (ITypeDef s) typeDefs)) l
 
 elaborateTDIdentType :: TypeDefs -> IdentType -> IdentType
 elaborateTDIdentType typeDefs t = 
