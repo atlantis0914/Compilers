1046c637141c8c06c391fe18d597a4833856d6b7
Peter Xiao <phx@andrew.cmu.edu>
2013-10-21 23:32:49 -0400

    add lab4

---
+++ lab4/runtime/15411.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* #include <stdbool.h> */
+/* using int for bool as of Oct 15, 2013 */
+
+typedef int c0_float;
+typedef int c0_bool;
+
+union float_or_int {
+  int as_int;
+  float as_float;
+};
+
+typedef union float_or_int float_or_int;
+
+c0_float fadd(c0_float a, c0_float b) {
+  float_or_int x; x.as_int = a;
+  float_or_int y; y.as_int = b;
+  float_or_int z; z.as_float = x.as_float + y.as_float;
+  return z.as_int;
+}
+
+c0_float fsub(c0_float a, c0_float b) {
+  float_or_int x; x.as_int = a;
+  float_or_int y; y.as_int = b;
+  float_or_int z; z.as_float = x.as_float - y.as_float;
+  return z.as_int;
+}
+
+c0_float fmul(c0_float a, c0_float b) {
+  float_or_int x; x.as_int = a;
+  float_or_int y; y.as_int = b;
+  float_or_int z; z.as_float = x.as_float * y.as_float;
+  return z.as_int;
+}
+
+c0_float fdiv(c0_float a, c0_float b) {
+  float_or_int x; x.as_int = a;
+  float_or_int y; y.as_int = b;
+  float_or_int z; z.as_float = x.as_float / y.as_float;
+  return z.as_int;
+}
+
+c0_bool fless(c0_float a, c0_float b) {
+  float_or_int x; x.as_int = a;
+  float_or_int y; y.as_int = b;
+  return x.as_float < y.as_float ? 1 : 0;
+}
+
+c0_float itof(int a) {
+  float_or_int x; x.as_float = (float)a;
+  return x.as_int;
+}
+
+int ftoi(c0_float a) {
+  float_or_int x; x.as_int = a;
+  return (int)x.as_float;
+}
+
+int print_fpt(c0_float a) {
+  float_or_int x; x.as_int = a;
+  fprintf(stderr, "%g\n", x.as_float);
+  return 0;
+}
+
+int print_int(int n) {
+  fprintf(stderr, "%d\n", n);
+  return 0;
+}
+
+int print_hex(int n) {
+  fprintf(stderr, "0x%08X\n", n);
+  return 0;
+}
