62873837bd25feb1dc23a03e310b6fca2e53f012
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-24 20:01:35 -0400

    adding size-lookup for types

--- lab4/compiler/src/Compile/Util/IdentTypeUtil.hs
+++ lab4/compiler/src/Compile/Util/IdentTypeUtil.hs
@@ -15,3 +15,18 @@ tM = [(IInt, IInt),
       (IVoid, IVoid)]
 
 baseIdentTypeMap = Map.fromList tM
+
+isStruct :: IdentType -> Bool
+isStruct (IStruct (ITypeDef _)) = True
+isStruct _ = False
+
+getSizeForType :: Map.Map String (Maybe SDefn) -> IdentType -> Int
+getSizeForType m IInt = 4
+getSizeForType m IBool = 4
+getSizeForType m IVoid = error ("Trying to get size for void")
+getSizeForType m (IPtr _) = 8
+getSizeForType m (IArray _) = 8
+getSizeForType m (IStruct name) = 
+  case (Map.lookup name m) of
+    Nothing -> error ("Struct " ++ name ++ " must be declared before use")
+    Just (SDefn {structSize = size}) = size
