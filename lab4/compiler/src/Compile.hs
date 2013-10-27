fd6ac28a10b6808f7758200c26ab3ebe13cb51e8
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-27 01:38:24 -0400

    Kind of gross, commented out compile.hs, but this is how far we've gotten haha.

--- lab4/compiler/src/Compile.hs
+++ lab4/compiler/src/Compile.hs
@@ -20,14 +20,14 @@ import Control.Monad.Error
 import Compile.Types
 import Compile.Frontend.Parse
 import Compile.Frontend.Elaborate
-import Compile.Frontend.TypeCheck
+-- import Compile.Frontend.TypeCheck
 import Compile.Frontend.CheckAST
-import Compile.Frontend.RenameFn
-import Compile.Frontend.ConstantFold
-import Compile.Frontend.RemVoid
-import Compile.IR.GenIR
-import Compile.Frontend.Minimize
-import Compile.Backend.CodeGen
+-- import Compile.Frontend.RenameFn
+-- import Compile.Frontend.ConstantFold
+-- import Compile.Frontend.RemVoid
+-- import Compile.IR.GenIR
+-- import Compile.Frontend.Minimize
+-- import Compile.Backend.CodeGen
 
 import qualified Debug.Trace as Trace
 
@@ -52,25 +52,26 @@ compile job = do
   res <- runErrorT $ do -- Constructor for the error monad transformer
     header <- getLibraryCode job
     (ParseFnList fnList pos) <- parseFnList $ jobSource job -- ParseFnList
-    let numFns = length fnList 
     elabFnList <- liftEIO $ elaborate (ParseFnList (header ++ fnList) pos) -- FnList
+    let numFns = length fnList 
     let (postCheckFnList, fnMap) = checkFnList elabFnList
-    let elabFnList'@(FnList tList _) = renameFn postCheckFnList
-    let elabFnList'' = (if ((length tList) > 50) -- Hacky shit to pass ../tests1/cobalt-return03.l3
-                          then elabFnList'
-                          else remFn elabFnList')
-    let elabFnList''' = (if (numFns == 1)
-                           then constantFold elabFnList''
-                           else elabFnList'')
-    minFnList <- liftEIO $ minimize elabFnList'''
-    if jobOutFormat job == C0
-      then writer (jobOut job) minFnList
-      else let asm = fnListCodeGen minFnList fnMap in
-              if jobOutFormat job == Asm
-                 then stringWriter (jobOut job) asm
-                 else do writer asmFile minFnList 
-                         let o = if jobOutFormat job == Obj then "-c" else ""
-                         gcc o asmFile (jobOut job)
+    writer (jobOut job) (postCheckFnList)
+--     let elabFnList'@(FnList tList _) = renameFn postCheckFnList
+--     let elabFnList'' = (if ((length tList) > 50) -- Hacky shit to pass ../tests1/cobalt-return03.l3
+--                           then elabFnList'
+--                           else remFn elabFnList')
+--     let elabFnList''' = (if (numFns == 1)
+--                            then constantFold elabFnList''
+--                            else elabFnList'')
+--     minFnList <- liftEIO $ minimize elabFnList'''
+--     if jobOutFormat job == C0
+--       then writer (jobOut job) minFnList
+--       else let asm = fnListCodeGen minFnList fnMap in
+--               if jobOutFormat job == Asm
+--                  then stringWriter (jobOut job) asm
+--                  else do writer asmFile minFnList 
+--                          let o = if jobOutFormat job == Obj then "-c" else ""
+--                          gcc o asmFile (jobOut job)
   case res of
     Left msg -> error msg
     Right () -> return ()
