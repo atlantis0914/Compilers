f15f50719591c9f129a0598ee07f979a8423b70b
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-27 18:13:15 -0400

    dat pretty printing yo

--- lab4/compiler/src/Compile/Types/IRExpr.hs
+++ lab4/compiler/src/Compile/Types/IRExpr.hs
@@ -20,4 +20,22 @@ data IRExpr = IRExpInt Integer Base
 -- We add back type information for [], . , and *.
             | IRExpArraySubscript IRExpr IRExpr IdentType Int
             | IRExpFieldSelect IRExpr String IdentType Int
-            | IRExpDereference IRExpr IdentType deriving Show 
+            | IRExpDereference IRExpr IdentType 
+
+instance Show IRExpr where
+  show (IRExpInt n _) = show n
+  show (IRExpBool b) = show b
+  show (IRIdent i) = i
+  show (IRExpNull) = "NULL"
+  show (IRExpBinOp op e1 e2) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (IRExpRelOp op e1 e2) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (IRExpPolyEq op e1 e2) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (IRExpLogOp op e1 e2) = "(" ++ show e1 ++ ") " ++ show op ++ " (" ++ show e2 ++ ")"
+  show (IRExpUnOp op e) = show op ++ "(" ++ show e ++ ")"
+  show (IRExpTernary e1 e2 e3) = show e1 ++ " ? " ++ show e2 ++ " : " ++ show e3
+  show (IRExpFnCall n elist) = "call " ++ n ++ "(" ++ (concatMap (\e -> show e ++ ",") elist) ++ ")"
+  show (IRExpAlloc typ off) = "alloc(" ++ show typ ++ "," ++ show off ++ ")"
+  show (IRExpAllocArray typ e off) = "alloc_array(" ++ show typ ++ "," ++ show e ++ "," ++ show off ++ ")"
+  show (IRExpArraySubscript e1 e2 typ off) = "(" ++ show e1 ++ "[" ++ show e2 ++ "]" ++ "," ++ show typ ++ "," ++ show off ++ ")"
+  show (IRExpFieldSelect e1 field typ off) = "(" ++ show e1 ++ "." ++ show field ++ "," ++ show typ ++ "," ++ show off ++ ")"
+  show (IRExpDereference e1 typ) = "(" ++ "*" ++ show e1 ++ "," ++ show typ ++ ")"
