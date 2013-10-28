503ce70921bf5967dceca9092524464c3ad1bc60
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-10-28 19:18:18 -0400

    making renaming checks explicit, catching rename edge case for Asgn

--- lab4/compiler/src/Compile/Frontend/RenameFn.hs
+++ lab4/compiler/src/Compile/Frontend/RenameFn.hs
@@ -44,7 +44,7 @@ renameBody (AST (Block stmts) pos) rnMap = AST (Block stmts') pos
     stmts' = map (renameStmt rnMap) stmts
 
 renameStmt :: RenameMap -> Stmt -> Stmt
-renameStmt rnMap (Asgn s op e b pos) = Asgn s op (renameExpr rnMap e) b pos
+renameStmt rnMap (Asgn (LExpr e1 p) op e2 b pos) = Asgn (LExpr (renameExpr rnMap e1) p) op (renameExpr rnMap e2) b pos
 renameStmt rnMap (Ctrl c) = Ctrl $ renameCtrl rnMap c
 renameStmt rnMap (Block stmts) = Block stmts'
   where
@@ -75,6 +75,11 @@ renameCtrl _ c = c
 
 renameExpr :: RenameMap -> Expr -> Expr
 
+renameExpr rm e@(ExpInt _ _ _) = e
+renameExpr rm e@(ExpBool _ _) = e
+renameExpr rm e@(Ident _ _) = e
+renameExpr rm e@(ExpNull _) = e
+renameExpr rm e@(ExpAlloc _ _) = e
 renameExpr rm e@(ExpBinOp o e1 e2 p) = ExpBinOp o (renameExpr rm e1) (renameExpr rm e2) p
 renameExpr rm e@(ExpRelOp o e1 e2 p) = ExpRelOp o (renameExpr rm e1) (renameExpr rm e2) p
 renameExpr rm e@(ExpPolyEq o e1 e2 p) = ExpPolyEq o (renameExpr rm e1) (renameExpr rm e2) p
@@ -82,8 +87,16 @@ renameExpr rm e@(ExpLogOp o e1 e2 p) = ExpLogOp o (renameExpr rm e1) (renameExpr
 renameExpr rm e@(ExpUnOp o e1 p) = ExpUnOp o (renameExpr rm e1) p
 renameExpr rm e@(ExpTernary e1 e2 e3 p) = ExpTernary (renameExpr rm e1) (renameExpr rm e2) (renameExpr rm e3) p
 renameExpr rm e@(ExpFnCall fnName expList p) = ExpFnCall (renameFnName rm fnName) (map (renameExpr rm) expList) p
-renameExpr rm e = e
+renameExpr rm e@(ExpAllocArray id e1 p) = ExpAllocArray id (renameExpr rm e1) p
+renameExpr rm e@(ExpBinMem o e1 e2 p) = ExpBinMem o (renameExpr rm e1) (renameExpr rm e2) p
+renameExpr rm e@(ExpUnMem o e1 p) = ExpUnMem o (renameExpr rm e1) p 
+renameExpr rm e = error ("Got : " ++ show e)
+-- renameExpr rm e = e
 
 -- Special cases for globally declared fns
 renameFnName rm ("main") = nameIfLibrary False ("main") -- main is never library
-renameFnName rm f = rm Map.! f
+renameFnName rm f = 
+  case (Map.lookup f rm) of 
+    Just t -> t
+    Nothing -> error ("Error couldn't find it") 
+-- renameFnName rm f = Trace.trace (" naming : " ++ (rm Map.! f)) $ rm Map.! f
