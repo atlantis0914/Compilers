f85d61f8a9a870e7b53422ebf01a16e36d6c468c
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2013-11-01 22:47:18 -0400

    Fixing getName - I think?

--- lab4/compiler/src/Compile/Util/IRUtil.hs
+++ lab4/compiler/src/Compile/Util/IRUtil.hs
@@ -2,7 +2,6 @@ module Compile.Util.IRUtil where
 
 
 getName :: String -> String
-getName (('_'):xs) = getName xs
-getName (('c'):('0'):('_'):xs) = getName xs
+--getName (('_'):xs) = getName xs
+getName (('_'):('c'):('0'):('_'):xs) = getName xs
 getName s = s
-
